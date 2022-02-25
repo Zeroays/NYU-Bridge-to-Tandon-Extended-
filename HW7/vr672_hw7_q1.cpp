6293f65440d9d3306c50784ed9a97ef889ec2b15
Vasu Rabaib <vrabaib@gmail.com>
2022-02-24 20:48:01 -0500

    Adding HW7

---
+++ HW7/vr672_hw7_q1.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+int printMonthCalendar(int numOfDays, int startingDay) {
+    const int DAYS_IN_A_WEEK = 7;
+    const string daysOfTheWeek = "Mon\tTue\tWed\tThr\tFri\tSat\tSun";
+
+    int upperBound, lowerBound;
+
+    int emptySpaces = 0;
+    for (; emptySpaces < startingDay - 1; emptySpaces++) {
+        cout << "\t";
+    }
+    int remaining = 0;
+    for (; remaining <= DAYS_IN_A_WEEK - startingDay; remaining++) {
+        cout << remaining + 1 << "\t";
+    }
+
+    cout << endl;
+
+
+    lowerBound = remaining + 1;
+    remaining++;
+    upperBound = lowerBound + DAYS_IN_A_WEEK;
+
+    for (; remaining <= numOfDays; remaining++) {
+        if (lowerBound <= remaining && upperBound > remaining) {
+            cout << remaining << "\t";
+        } else {
+            cout << endl;
+            cout << remaining << "\t";
+            lowerBound = upperBound;
+            upperBound = lowerBound + DAYS_IN_A_WEEK;
+        }
+    }
+    return numOfDays - lowerBound + 1;
+
+}
+
+int isLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 != 0) {
+            return true;
+        } else {
+            if (year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
+void printYearCalendar(int year, int startingDay) {
+    const int DAYS_IN_A_WEEK = 7;
+    const int MONTHS_IN_A_YEAR = 12;
+
+    const int FEBRUARY = 2;
+    const int APRIL = 4;
+    const int JUNE = 6;
+    const int SEPTEMBER = 9;
+    const int NOVEMBER = 11;
+
+    for (int month = 1; month <= MONTHS_IN_A_YEAR; month++) {
+        int endingDay;
+        int daysInAMonth;
+
+        switch (month) {
+            case 1: cout << "January"; break;
+            case 2: cout << "February"; break;
+            case 3: cout << "March"; break;
+            case 4: cout << "April"; break;
+            case 5: cout << "May"; break;
+            case 6: cout << "June"; break;
+            case 7: cout << "July"; break;
+            case 8: cout << "August"; break;
+            case 9: cout << "September"; break;
+            case 10: cout << "October"; break;
+            case 11: cout << "November"; break;
+            case 12: cout << "December"; break;
+        }
+        cout << " " << year << endl;
+
+        if (month == APRIL || month == JUNE || month == SEPTEMBER || month == NOVEMBER) {
+            daysInAMonth = 30;
+        } else if (month == FEBRUARY) {
+            if (isLeapYear(year)) {
+                daysInAMonth = 29;
+            } else {
+                daysInAMonth = 28;
+            }  
+        } else {
+            daysInAMonth = 31;
+        }
+
+        endingDay = printMonthCalendar(daysInAMonth, startingDay);
+        startingDay = endingDay + 1;
+        if (startingDay > DAYS_IN_A_WEEK) {
+            startingDay = 1;
+        }
+        
+        cout << endl << endl;
+    }
+    
+
+}
+
+int main() {
+    int lastDay;
+    int year;
+
+    lastDay = printMonthCalendar(31, 1);
+    cout << endl;
+    cout << lastDay << endl;
+
+    year = isLeapYear(1904);
+    cout << year << endl;
+
+    printYearCalendar(2016, 5);
+
+    return 0;
+}
---
+++ HW7/vr672_hw7_q2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void analyzeDividors(int num, int& outCountDivs, int& outSumDivs) {
+    int countDivs = 0, sumDivs = 1;
+
+    for (int i = 2; i * i < num; i++) {
+        if (num % i == 0) {
+            sumDivs += i;
+            sumDivs += num / i;
+            countDivs += 2;
+        }
+    }
+    outCountDivs = countDivs;
+    outSumDivs = sumDivs;
+}
+
+bool isPerfect(int num) {
+    int countDivs, sumDivs;
+    analyzeDividors(num, countDivs, sumDivs);
+    return num == sumDivs;
+}
+
+int main() {
+    int num;
+    
+    cout << "Please provide a positive integer >= 2: ";
+    cin >> num;
+
+    cout << endl;
+    cout << "***Perfect Numbers between 2 and " << num << "***" << endl;
+
+    for (int i = 2; i < num; i++) {
+        if (isPerfect(i)) {
+            cout << i << endl;
+        }
+    }
+
+    cout << endl << endl;
+
+    cout << "***Pairs of Amicable Numbers between 2 and " << num << "***" << endl;
+    for (int i = 2; i < num; i++) {
+        int countDivs1, sumDivs1;
+        int countDivs2, sumDivs2;
+
+        analyzeDividors(i, countDivs1, sumDivs1);
+        analyzeDividors(sumDivs1, countDivs1, sumDivs2);
+
+        if (i == sumDivs2 && i < sumDivs1) {
+            cout << i << ", " << sumDivs1 << endl;
+        }
+    }
+
+    return 0;
+}
